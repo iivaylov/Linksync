7ff02953866c2ce83fbb10cabfc87e7b937fb52b
Ivaylo Iliev <ilievivaylo200@proton.me>
2023-08-08 18:08:15 +0300

    Feature for user updates

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,15 +4,24 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
 
- export async function updateUser(userId: string, username: string, name: string, bio: string, image: string, path: string): Promise<void> {
-    connectToDB();
+interface Params {
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+}
 
-   try{
-    await User.findOneAndUpdate({id: userId}, {username: username.toLowerCase(), name, bio, image, onboarded: true}, {upsert: true});
-    if(path === '/profile/edit') {
-        revalidatePath(path);
+export async function updateUser({ userId, username, name, bio, image, path }: Params): Promise<void> {
+  connectToDB();
+
+  try {
+    await User.findOneAndUpdate({ id: userId }, { username: username.toLowerCase(), name, bio, image, onboarded: true }, { upsert: true });
+    if (path === '/profile/edit') {
+      revalidatePath(path);
     }
-   }catch(error: any){
+  } catch (error: any) {
     throw new Error(`Failed to create/update user: ${error.message}`)
-   }
- }
+  }
+}
