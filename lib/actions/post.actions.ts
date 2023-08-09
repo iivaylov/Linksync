56984c780b361d9d5c08d9cc46fb161a8634d7a1
Ivaylo Iliev <ilievivaylo200@proton.me>
2023-08-09 22:31:22 +0300

    Showing created posts on home page

--- lib/actions/post.actions.ts
+++ lib/actions/post.actions.ts
@@ -13,13 +13,36 @@ interface Params {
 }
 
 export async function createPost({ text, author, communityId, path }: Params) {
-    try{
+    try {
         connectToDB();
         const createPost = await Post.create({ text, author, community: null });
         //Update user model
-        await User.findByIdAndUpdate(author, {$push : { posts: createPost._id}});
+        await User.findByIdAndUpdate(author, { $push: { posts: createPost._id } });
         revalidatePath(path);
-    }catch(error: any){
+    } catch (error: any) {
         throw new Error(`Error creating post: ${error.message}`)
     }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    //Calculate the number of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    //Fetch the posts that do not have parents (top-level posts...)
+    const postsQuery = Post.find({ parentId: { $in: [null, undefined] } })
+        .sort({ createdAt: 'desc' })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: User })
+        .populate({ path: 'children', populate: { path: 'author', model: User, select: "_id name parentId image" } })
+
+    const totalPostsCount = await Post.countDocuments({ parentId: { $in: [null, undefined] } });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext }
 }
