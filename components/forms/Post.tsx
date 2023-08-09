2ffd59da43bf4c9d7a09a1cfd2cc9d284a782ac5
Ivaylo Iliev <ilievivaylo200@proton.me>
2023-08-09 21:27:46 +0300

    Add post creation feature

--- components/forms/Post.tsx
+++ components/forms/Post.tsx
@@ -8,6 +8,9 @@ import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { usePathname, useRouter } from 'next/navigation';
+import { PostValidation } from "@/lib/validations/post";
+import { createPost } from "@/lib/actions/post.actions";
+import { getRandomValues } from "crypto";
 
 // import { updateUser } from "@/lib/actions/user.actions";
 // import { UserValidation } from "@/lib/validations/user";
@@ -27,15 +30,42 @@ function Post({ userId }: { userId: string }) {
   const router = useRouter();
   const pathname = usePathname();
 
-  const form = useForm<z.infer<typeof >>({
-    resolver: zodResolver(),
+  const form = useForm<z.infer<typeof PostValidation>>({
+    resolver: zodResolver(PostValidation),
     defaultValues: {
       post: '',
       accountId: userId,
     }
   });
 
-    return <h1>Post Form</h1>
+  const onSubmit = async (values: z.infer<typeof PostValidation>) => {
+    await createPost({
+        text: values.post, 
+        author: userId,
+        communityId: null,
+        path: pathname
+    });
+
+    router.push('/');
+  }
+
+    return (
+    <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="mt-10 flex flex-col justify-start gap-10">
+        <FormField control={form.control} name="post" render={({ field }) => (
+            <FormItem className="flex flex-col w-full gap-3">
+              <FormLabel className="text-base-semibold text-light-2">Content</FormLabel>
+              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                <Textarea rows={5} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="bg-primary-500">Publish</Button>
+        </form>
+    </Form>
+    )
 }
 
 export default Post;
