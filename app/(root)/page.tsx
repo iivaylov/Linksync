57610669210bbd5e8ea7c93d397c938a8b4bfdf2
Ivaylo Iliev <ilievivaylo200@proton.me>
2023-08-25 21:12:52 +0300

    Add communities page view

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,6 +9,7 @@ import { fetchUser } from "@/lib/actions/user.actions";
 export default async function Home() {
   const user = await currentUser();
   if (!user) return null;
+  // if(!user) redirect('/sign-in');
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
